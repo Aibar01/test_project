30c4f8bc002f6c8198c3cecfb8b546296c5415fc
Aibar01 <bekaybar@gmail.com>
2020-09-01 18:27:44 +0600

    models added to admin page

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    list_display_links = ('id', 'title')
+    search_fields = ('title',)
+
